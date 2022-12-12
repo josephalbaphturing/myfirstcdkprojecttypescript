5185f895ab8b76c89c80055032c8b8b666210118
josephalbaph-at-617400109433 <josephalbaph-at-617400109433>
2022-12-12 13:56:19 -0500

    My First Commit

--- lib/my-first-cdk-stack.ts
+++ lib/my-first-cdk-stack.ts
@@ -1,10 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyFirstCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    new s3.Bucket(this, 'MyFirstCdkBucket', {
+      bucketName: 'myfirstcdkprojectjalbaph123',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      versioned: true,
+      encryption: s3.BucketEncryption.KMS_MANAGED,
+    });
 
     // The code that defines your stack goes here
 
